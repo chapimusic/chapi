ac26e7457ef4762dd693e70321f4e7d34845e9f7
Joris <joris@saidani.org>
2020-08-05 20:11:42 +0200

    undo

--- src/pages/concerts.js
+++ src/pages/concerts.js
@@ -24,14 +24,22 @@ const ConcertsPage = () => {
   return (
     <Layout>
       <SEO title="Concerts" />
-      <Box sx={{p: 4, maxWidth: '960px', mx: 'auto'}}>
-        <Box sx={{bg: 'red.1', color: 'white', p: 3}}>
-          <Styled.h2>Concerts à venir</Styled.h2>{' '}
-          <BlockContent blocks={_rawUpcomingConcerts} />
-        </Box>
-        <Box sx={{pt: 3}}>
-          <Styled.h2>Concerts passés</Styled.h2>
-          <BlockContent blocks={_rawPastConcerts} />
+      <Box
+        sx={{
+          position: 'relative',
+          background: 'url(/arc.svg)',
+          height: '100%',
+        }}
+      >
+        <Box sx={{p: 4, maxWidth: '960px', mx: 'auto'}}>
+          <Box sx={{bg: 'red.1', color: 'white', p: 3}}>
+            <Styled.h2>Concerts à venir</Styled.h2>{' '}
+            <BlockContent blocks={_rawUpcomingConcerts} />
+          </Box>
+          <Box sx={{pt: 3}}>
+            <Styled.h2>Concerts passés</Styled.h2>
+            <BlockContent blocks={_rawPastConcerts} />
+          </Box>
         </Box>
       </Box>
     </Layout>
