da1d41cddd4df07cf85f854df235265be2aee7ef
Joris <joris@saidani.org>
2020-08-06 19:49:06 +0200

    contact en haut

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -17,19 +17,19 @@ export const Contact = props => {
   `)
   return (
     <Grid gap={4} columns={[1]} sx={{maxWidth: '400px', mx: 'auto'}} {...props}>
+      <Box sx={{textAlign: 'center', bg: 'white', p: 3}}>
+        <Styled.h1 css={{marginBlockStart: 0, marginBlockEnd: 0}}>
+          <span sx={{fontSize: 4}}>Contact</span>
+        </Styled.h1>
+        <p sx={{fontSize: 3, pb: 4}}>
+          <a href={`mailto:contact@chapimusic.com`}>contact@chapimusic.com</a>
+        </p>
+      </Box>
       <Box sx={{textAlign: 'center'}}>
         <Img
           fluid={data.jouetSynth.childImageSharp.fluid}
           sx={{maxWidth: '100%'}}
         />
-      </Box>
-      <Box sx={{textAlign: ['left', 'center'], bg: 'white', p: 3}}>
-        <Styled.h1 css={{marginBlockStart: 0, marginBlockEnd: 0}}>
-          <span sx={{fontSize: 4}}>Contact</span>
-        </Styled.h1>
-        <p sx={{fontSize: 3, pb: 5}}>
-          <a href={`mailto:contact@chapimusic.com`}>contact@chapimusic.com</a>
-        </p>
       </Box>
     </Grid>
   )
